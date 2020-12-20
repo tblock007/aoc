2fda5cf34c4475e487ed60ba814419daa6e0c9b2
Michael Wong <the.t.block@gmail.com>
2020-12-20 10:23:29 -0800

    Upload WIP D19P1.

---
+++ 2020/D19p1.hs
@@ -0,0 +1,33 @@
+import D19input
+import qualified Data.IntMap.Lazy as Map
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
+
+type Rule = [[Int]]
+
+stripRule :: String -> Map.IntMap Rule -> Int -> [String]
+stripRule "" _ _ = []
+stripRule s m index
+    | index == aRule = if head s == 'a' then [tail s] else []
+    | index == bRule = if head s == 'b' then [tail s] else []
+    | otherwise =
+        let (Just r) = Map.lookup index m
+         in concat $ List.map (stripRuleSeq s m) r
+
+stripRuleSeq :: String -> Map.IntMap Rule -> [Int] -> [String]
+stripRuleSeq s m [] = [s]
+stripRuleSeq s m (i:is) = do
+    stripped <- stripRule s m i
+    stripRuleSeq stripped m is
+
+matchesRule :: Map.IntMap Rule -> Int -> String -> Bool
+matchesRule m i s =
+    let tails = stripRule s m i
+     in List.elem "" tails
+
+solve :: Int
+solve = let rulesMap = Map.fromList rules
+         in length $ filter (matchesRule rulesMap 0) input
+
+main :: IO ()
+main = print $ solve
